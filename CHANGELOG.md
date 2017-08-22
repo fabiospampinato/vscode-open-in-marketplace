df6dd471dc9f4e5dbf23e551a7b62dc5f759b30a
Fabio Spampinato <spampinabio@gmail.com>
2017-08-22 23:15:09 +0200

    Added support for walking up the tree

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+### Version 1.1.0
+- Added support for walking up the tree
+
 ### Version 1.0.0
 - Initial release
--- src/commands.ts
+++ src/commands.ts
@@ -2,6 +2,7 @@
 /* IMPORT */
 
 import * as _ from 'lodash';
+import * as absolute from 'absolute';
 import * as openPath from 'open';
 import * as path from 'path';
 import * as vscode from 'vscode';
@@ -15,14 +16,38 @@ async function open () {
 
   if ( !rootPath ) return vscode.window.showErrorMessage ( 'You have to open a project before being able to open it in the Marketplace' );
 
-  const packagePath = path.join ( rootPath, 'package.json' ),
-        packageContent = await Utils.file.read ( packagePath ),
-        packageObj = _.attempt ( JSON.parse, packageContent ),
-        isMarketplaceExtension = !_.isError ( packageObj ) && _.isPlainObject ( packageObj.engines ) && packageObj.engines.vscode && packageObj.publisher && packageObj.name;
+  let projectPath = await Utils.folder.getWrapperPath ( rootPath, rootPath, 'package.json' );
 
-  if ( !isMarketplaceExtension ) return vscode.window.showErrorMessage ( 'This project doesn\'t look like a Marketplace extension' );
+  if ( !projectPath ) { // Walk upwards from the currently open file
 
-  openPath ( `https://marketplace.visualstudio.com/items?itemName=${packageObj.publisher}.${packageObj.name}` );
+    const {activeTextEditor} = vscode.window,
+          editorPath = activeTextEditor && activeTextEditor.document.fileName,
+          folderPath = editorPath && absolute ( editorPath ) && path.dirname ( editorPath );
+
+    if ( folderPath ) {
+
+      projectPath = await Utils.folder.getWrapperPath ( rootPath, folderPath, 'package.json' );
+
+    }
+
+  }
+
+  if ( projectPath ) {
+
+    const packagePath = path.join ( projectPath, 'package.json' ),
+          packageContent = await Utils.file.read ( packagePath ),
+          packageObj = _.attempt ( JSON.parse, packageContent ),
+          isMarketplaceExtension = !_.isError ( packageObj ) && _.isPlainObject ( packageObj.engines ) && packageObj.engines.vscode && packageObj.publisher && packageObj.name;
+
+    if ( isMarketplaceExtension ) {
+
+      return openPath ( `https://marketplace.visualstudio.com/items?itemName=${packageObj.publisher}.${packageObj.name}` );
+
+    }
+
+  }
+
+  vscode.window.showErrorMessage ( 'This project doesn\'t look like a Marketplace extension' );
 
 }
 
